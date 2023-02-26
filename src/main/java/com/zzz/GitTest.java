c2c245e87430130fb1a71d1213bf85d627810d72
Ashen <wfz8016@gmail.com>
2023-02-26 14:40:55 +0800

    first commit test

---
+++ src/main/java/com/zzz/GitTest.java
@@ -0,0 +1,13 @@
+package com.zzz;
+
+/**
+ * @author Ashen
+ * @program git-demo
+ * @description
+ * @data 2023-02-26 14:38
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
